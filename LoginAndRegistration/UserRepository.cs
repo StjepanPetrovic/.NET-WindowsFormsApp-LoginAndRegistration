37ef376ef00ba33a636dc53e6ea38c98a082dec2
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2022-10-09 21:16:21 +0200

    Implementacija poslovne logike

---
+++ LoginAndRegistration/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginAndRegistration
+{
+    public class UserRepository
+    {
+        public static List<User> users = new List<User>();
+    }
+}
